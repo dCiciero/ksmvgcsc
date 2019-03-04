c2ef6b9e8c06ffd012dfd1aae0d1c92586f74a86
Ogaga <ivhurie01@yahoo.co.uk>
2019-03-04 14:12:55 +0100

    Update procfile

--- vgcsc/__init__.py
+++ vgcsc/__init__.py
@@ -6,15 +6,17 @@ from config import Config
 from flask_login import LoginManager
 
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+    login = LoginManager(app)
+    login.login_view = 'login'
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+    from vgcsc import routes, models
 
-from vgcsc import routes, models
+    return app
 # . import models, routes
 
 # app.register_blueprint(routes.bp)
